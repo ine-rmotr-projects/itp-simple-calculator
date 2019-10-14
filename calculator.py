ef1afb43d1ee81af83b2ec3d8dbe60f2244ba90f
itoffensedefense <36059132+itoffensedefense@users.noreply.github.com>
2019-10-14 19:54:07 +0200

    Update calculator.py
    Third attempt.

--- calculator.py
+++ calculator.py
@@ -1,26 +1,32 @@
 def add(x, y):
-    pass
+    return x + y
 
 
 def subtract(x, y):
-    pass
+    return x - y
+
 
 
 def divide(x, y):
-    pass
+    if y == 0:
+        return "Invalid operator, can't divide by 0"
+    else:
+        return x / y
 
 
 def multiply(x, y):
-    pass
+    return x * y
+
 
 
 def square(x):
-    pass
+    return x ** 2
 
 
 def power(x, y):
-    pass
+    return x ** y
 
-
+import math
 def sqrt(x):
-    pass
+
+    return math.sqrt(x)
