b2191d9b62636778d8f76a97fa90b635f8974832
itoffensedefense <36059132+itoffensedefense@users.noreply.github.com>
2019-10-14 19:21:23 +0200

    itoffensedefense_calculator.py
    This is my updated calculator

---
+++ itoffensedefense_calculator.py
@@ -0,0 +1,36 @@
+def add(x, y):
+    return x + y
+
+# For the add() function
+$ py.test test_add.py
+
+def subtract(x, y):
+    return x - y
+
+# For the subtract() function
+$ py.test test_subtract.py
+
+def divide(x, y):
+    if y = 0:
+        return 0
+    else:
+        return x / y
+$ py.test test_divide.py
+
+def multiply(x, y):
+    return x * y
+
+# For the mulitply() function
+$ py.test test_multiply.py
+
+def square(x):
+    return x ** 2
+$ py.test test_square.py
+
+def power(x, y):
+    return x ** y
+$ py.test test_power.py
+
+def sqrt(x):
+    return x ** x
+$ py.test test_sqrt.py
