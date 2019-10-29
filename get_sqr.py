648c5474b4ca893bd5bcbf13d9d673f59e9f3592
Amitupadhyay368 <amitkumarupadhyay368@gmail.com>
2019-10-29 14:40:17 +0530

    changes in function name or variable name or values or in results in square

---
+++ get_sqr.py
@@ -0,0 +1,6 @@
+from calculator import square
+
+
+def get_sqr():  #variable name cahnges from test_square to get_sqr
+    output = square(15)#value changes from 3 to 15
+    assert output == 225#square become 225
--- test_square.py
+++
@@ -1,6 +0,0 @@
-from calculator import square
-
-
-def test_square():
-    results = square(3)
-    assert results == 9
