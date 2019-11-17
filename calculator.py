4d000c70a558ba809a7e29ec8f200d92b12d41ad
perbor <57865387+perbor@users.noreply.github.com>
2019-11-17 15:04:13 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,26 +1,28 @@
 def add(x, y):
-    pass
+    return x + y
 
 
 def subtract(x, y):
-    pass
+    return x - y
 
 
 def divide(x, y):
-    pass
+    if y == 0:
+    	return "Invalid value for denominator, cant't divide by 0!"
+    return x / y
 
 
 def multiply(x, y):
-    pass
+    return x * y
 
 
 def square(x):
-    pass
+    x * x 
 
 
 def power(x, y):
-    pass
+    x ** y
 
 
 def sqrt(x):
-    pass
+    x ** 0.5
