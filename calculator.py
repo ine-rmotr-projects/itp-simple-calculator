fb027a719945d1fe1fd55bc68129d80d66b80896
John.CE <KeepitSimple612@users.noreply.github.com>
2019-12-03 21:52:42 -0500

    MyUpdate calculator.py
    Please review my file. Thank you

--- calculator.py
+++ calculator.py
@@ -1,26 +1,28 @@
 def add(x, y):
-    pass
+    return x + y
 
 
 def subtract(x, y):
-    pass
+    return x - y
 
 
 def divide(x, y):
-    pass
-
+    if (y == 0):
+        return 'Invalid value for denominator, cant\'t divide by 0!'
+    else:
+        return x/y
 
 def multiply(x, y):
-    pass
+    return x*y
 
 
 def square(x):
-    pass
+    return x**2
 
 
 def power(x, y):
-    pass
+    return x**y
 
 
 def sqrt(x):
-    pass
+    return x**(0.5)
