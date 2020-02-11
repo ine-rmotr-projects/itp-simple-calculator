28856b58780cde68d7a759d9d5db4ab6382a973c
jhmbtj <60899943+jhmbtj@users.noreply.github.com>
2020-02-11 11:22:18 -0500

    Calculator Solution
    Thank you

--- calculator.py
+++ calculator.py
@@ -1,26 +1,29 @@
 def add(x, y):
-    pass
+    retun x + y
 
 
 def subtract(x, y):
-    pass
+    return x - y
 
 
 def divide(x, y):
-    pass
+    if y == 0:
+        return "Invalid value for denominator, cant't divide by 0!"
+    elif y != 0:
+        return x / y
 
 
 def multiply(x, y):
-    pass
+    return x * y
 
 
 def square(x):
-    pass
+    return x * x
 
 
 def power(x, y):
-    pass
+    return x ** y
 
 
 def sqrt(x):
-    pass
+    return x ** 0.5
