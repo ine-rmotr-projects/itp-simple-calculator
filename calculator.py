b8ee3c68238d7876a4e414f2dc0a4152bc1a3dd3
William Ponton <waponton@gmail.com>
2020-02-29 21:10:39 -0500

    Initial commit

--- calculator.py
+++ calculator.py
@@ -1,26 +1,40 @@
 def add(x, y):
-    pass
+    addition = 0
+    addition = int(x + y)
+    return addition
 
 
 def subtract(x, y):
-    pass
+    subtraction = 0
+    subtraction = int(x - y)
+    return subtraction
 
 
 def divide(x, y):
-    pass
-
+    division = 0
+    if x == 0 or y ==0:
+        division = "Error - cannot divide by zero!"
+        return division
+    else:
+        division = int(x / y)
+    return division
 
 def multiply(x, y):
-    pass
-
+    multiplication = 0
+    multiplication = int(x * y)
+    return multiplication
 
 def square(x):
-    pass
-
+    squared = 0
+    squared = int(abs(x ** 2))
+    return squared
 
 def power(x, y):
-    pass
-
+    exponent = 0
+    exponent = int(x ** (y))
+    return exponent
 
 def sqrt(x):
-    pass
+    square_root = 0
+    square_root = int(x **(.5))
+    return square_root
--- test_add.py
+++ test_add.py
@@ -4,3 +4,4 @@ from calculator import add
 def test_add():
     results = add(1, 2)
     assert results == 3
+
