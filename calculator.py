ef48893f623f27f30fc461c192e8edf3e66ab6ac
Caleb <calebsmacgregor@gmail.com>
2020-03-08 09:37:01 +1000

    built body of functions

--- calculator.py
+++ calculator.py
@@ -1,26 +1,29 @@
 def add(x, y):
-    pass
+    return x + y
 
 
 def subtract(x, y):
-    pass
+    return x - y
 
 
 def divide(x, y):
-    pass
+    if y != 0:
+        return x / y
+    else:
+        return "Invalid value for denominator, can't divide by 0!"
 
 
 def multiply(x, y):
-    pass
+    return x * y
 
 
 def square(x):
-    pass
+    return x ** 2
 
 
 def power(x, y):
-    pass
+    return x ^ y
 
 
 def sqrt(x):
-    pass
+    return sqrt(x)
