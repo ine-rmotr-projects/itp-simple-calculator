fa604643d128091b55490705e6f2d1f5d246d1c9
plasmagirl <neha.nehru@uky.edu>
2020-03-17 21:56:36 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,26 +1,29 @@
 def add(x, y):
-    pass
+    return x+y
 
 
 def subtract(x, y):
-    pass
+    return x-y
 
 
 def divide(x, y):
-    pass
+    if y==0:
+        print('Cannot divide by 0')
+        return
+    return x/y
 
 
 def multiply(x, y):
-    pass
+    return x*y
 
 
 def square(x):
-    pass
+    return x**2
 
 
 def power(x, y):
-    pass
+    return x**y
 
 
 def sqrt(x):
-    pass
+    return x ** 0.5
