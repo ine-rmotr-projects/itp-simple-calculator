ce24030b1cf1f2669103b591897538cde20db55b
hannibal164 <mmcbride164@hotmail.com>
2020-04-05 23:21:58 +0200

    Updated calculator.py

--- calculator.py
+++ calculator.py
@@ -1,26 +1,29 @@
 def add(x, y):
-    pass
+    return x + y
 
 
 def subtract(x, y):
-    pass
+    return x - y
 
 
 def divide(x, y):
-    pass
+	if y == 0:
+		return "Denominator cannot be 0"
+	else:
+		return x / y
 
 
 def multiply(x, y):
-    pass
+    return x * y
 
 
 def square(x):
-    pass
+    return x ** 2
 
 
 def power(x, y):
-    pass
+    return x ** y
 
 
 def sqrt(x):
-    pass
+    return x ** 0.5
