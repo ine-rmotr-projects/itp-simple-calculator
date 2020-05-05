da5222868d9e59d0d12d50bf844edaa17568c1f4
wvuboy21 <64705903+wvuboy21@users.noreply.github.com>
2020-05-04 23:18:30 -0400

    Calculator Solution
    Calculator Solution

--- calculator.py
+++ calculator.py
@@ -1,26 +1,29 @@
 def add(x, y):
-    pass
+    return x + y
 
 
 def subtract(x, y):
-    pass
+    return x - y
 
 
 def divide(x, y):
-    pass
+    if y != 0:
+        return x/y
+    else:
+        return "Don't divide by zero"
 
 
 def multiply(x, y):
-    pass
+    return x*y
 
 
 def square(x):
-    pass
+    return x**2
 
 
 def power(x, y):
-    pass
+    return x**y
 
 
 def sqrt(x):
-    pass
+    return x**.5
