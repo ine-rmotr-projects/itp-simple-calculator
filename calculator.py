39d77e5205668a80c555f447f3a904a7ba0b9f9b
Mark R. Baker <mrbaker1917@gmail.com>
2020-05-15 18:09:16 -0700

    coded all operations

--- calculator.py
+++ calculator.py
@@ -1,26 +1,49 @@
 def add(x, y):
-    pass
+    if type(x) != int or type(y) != int:
+        return None
+    else:
+        return x + y
 
 
 def subtract(x, y):
-    pass
+    if type(x) != int or type(y) != int:
+        return None
+    else:
+        return x - y
 
 
 def divide(x, y):
-    pass
+    if type(x) != int or type(y) != int:
+        return None
+    elif y == 0:
+        return 'Error: cannot divide by zero!'
+    else:
+        return x / y
 
 
 def multiply(x, y):
-    pass
+    if type(x) != int or type(y) != int:
+        return None
+    else:
+        return x * y
 
 
 def square(x):
-    pass
+    if type(x) != int:
+        return None
+    else:
+        return x**2
 
 
 def power(x, y):
-    pass
+    if type(x) != int or type(y) != int:
+        return None
+    else:
+        return x**y
 
 
 def sqrt(x):
-    pass
+    if type(x) != int:
+        return None
+    else:
+        return x**(0.5)
