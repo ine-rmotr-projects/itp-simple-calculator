179ec592fad76c67c8260a2d10442ea6c791e56c
benwbarr <65369591+benwbarr@users.noreply.github.com>
2020-05-17 13:49:30 -0700

    Calculator solution
    Code for simple calculator

--- calculator.py
+++ calculator.py
@@ -1,26 +1,27 @@
 def add(x, y):
-    pass
+    return x + y
 
 
 def subtract(x, y):
-    pass
+    return x - y
 
 
 def divide(x, y):
-    pass
-
+    if y == 0:
+        return "Can't divide by zero."
+    return x / y
 
 def multiply(x, y):
-    pass
+    return x * y
 
 
 def square(x):
-    pass
+    return x * x
 
 
 def power(x, y):
-    pass
+    return x ** y
 
 
 def sqrt(x):
-    pass
+    return power(x, 1/2)
