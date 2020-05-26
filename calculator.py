3949569fa7e98a8799dac3f17cc989ea1c79f9dd
bayes-was-probably-right <33172214+johannoh@users.noreply.github.com>
2020-05-26 23:54:55 +0800

    Solution to calculator.py

--- calculator.py
+++ calculator.py
@@ -1,26 +1,28 @@
 def add(x, y):
-    pass
+    return x + y
 
 
 def subtract(x, y):
-    pass
+    return x - y
 
 
 def divide(x, y):
-    pass
+    if y == 0:
+        return "Invalid input - cannot divide by 0."
+    return x / y
 
 
 def multiply(x, y):
-    pass
+    return x * y
 
 
 def square(x):
-    pass
+    return x * x
 
 
 def power(x, y):
-    pass
+    return x ** y
 
 
 def sqrt(x):
-    pass
+    return power (x, 1/2)
