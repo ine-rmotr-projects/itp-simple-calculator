c140736da677e914446ef0ddf0b010b9e1aaa5b2
dcraker10 <56006196+dcraker10@users.noreply.github.com>
2020-06-04 13:26:13 -0700

    add warning string

--- calculator.py
+++ calculator.py
@@ -7,7 +7,10 @@ def subtract(x, y):
 
 
 def divide(x, y):
-    return x / y
+    if y == 0:
+        return "Invalid value for denominator, cant't divide by 0!"
+    else:
+        return x / y
 
 
 def multiply(x, y):
