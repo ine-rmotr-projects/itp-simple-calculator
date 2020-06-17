1627539fe954c081f9424af0f73a09685bb66fce
Shannon K'doah Range <51458305+srange123@users.noreply.github.com>
2020-06-17 14:30:59 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,26 +1,29 @@
 def add(x, y):
-    pass
+    return x + y
 
 
 def subtract(x, y):
-    pass
+    return x - y
 
 
 def divide(x, y):
-    pass
+    # check if y is 0, there should be a test for that
+    if y == 0:
+        return "Invalid value. Can't divide by zero."
+    return x / y
 
 
 def multiply(x, y):
-    pass
+    return x * y
 
 
 def square(x):
-    pass
+    return x ** 2  # Alternatives: x * x, pow (x, 2)
 
 
 def power(x, y):
-    pass
+    return x ** y
 
 
 def sqrt(x):
-    pass
+    return power(x, 1/2)
