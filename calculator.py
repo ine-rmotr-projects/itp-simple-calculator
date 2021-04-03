3962bbf8569d1c971eac2273237efdc242b55b9a
Ahmed Adel <54274975+DoulaV@users.noreply.github.com>
2021-04-04 00:57:30 +0200

    Update the Calculator

--- calculator.py
+++ calculator.py
@@ -3,24 +3,25 @@ def add(x, y):
 
 
 def subtract(x, y):
-    pass
+    return x - y
 
 
 def divide(x, y):
-    pass
+    if (y == 0) print("Invalid value for denominator")
+    else return x / y
 
 
 def multiply(x, y):
-    pass
+    return x * y 
 
 
 def square(x):
-    pass
-
+    return x * x
+   
 
 def power(x, y):
-    pass
+    return x ** y
 
 
 def sqrt(x):
-    pass
+    x ** (0.5)
