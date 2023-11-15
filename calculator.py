aa81ac8f9b66a6ca4dd9f38afcd798fdc75e881f
rjencinas <150954110+rjencinas@users.noreply.github.com>
2023-11-15 11:20:06 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,24 +3,24 @@ def add(x, y):
 
 
 def subtract(x, y):
-    pass
+    return x - y
 
 
 def divide(x, y):
-    pass
+    return x / y
 
 
 def multiply(x, y):
-    pass
+    return x * y
 
 
 def square(x):
-    pass
+    return x ^ 2
 
 
 def power(x, y):
-    pass
+    return x ^ y
 
 
 def sqrt(x):
-    pass
+    return sqrt(x)
